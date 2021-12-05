a5f49231aface523e5b89301d6ba5a13c20ff1d8
Abdulmateen <jolaoshobatmat@gmail.com>
2021-12-05 22:28:39 +0100

    array sortings

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,13 +12,16 @@
                     <h5 class="mont text-start text-white">
                         Filter Results
                     </h5>
-                    <form class="text-start my-5">
 
+
+
+
+                    <form class="text-start my-5">
                         <div class="">
                             <label for="name" class="text-white">
                                 Name(contains)
                             </label>
-                            <input placeholder="Text String" class="form-control text-white rounded-0 muli bg-input border-0 my-2 py-2">
+                            <input placeholder="Text String" v-model="name" class="form-control text-white rounded-0 muli bg-input border-0 my-2 py-2" @keyup="SearchbyName">
                         </div>
 
                         <br>
@@ -30,7 +33,7 @@
                                     <label for="name" class="text-white">
                                         Minimum Score
                                     </label>
-                                    <input placeholder="1 - 10" class="form-control text-white rounded-0 muli bg-input border-0 mt-2 py-2">
+                                    <input placeholder="1 - 10" @keyup="sortByMinScore()" v-model="min_score" class="form-control text-white rounded-0 muli bg-input border-0 mt-2 py-2">
                                 </div>
                             </div>
 
@@ -41,12 +44,15 @@
                                         <button class="btn bg-button rounded-0">
                                             <Icon icon="dashicons:arrow-up-alt" class="text-white" />
                                         </button>
-                                        <select placeholder="Release Date" class="form-control rounded-0 border-0" id="select">
-                                            <option>
+                                        <select placeholder="Order By" v-model="order_by" class="form-control rounded-0 border-0" id="select">
+                                            <option value="score">
                                                 Score
                                             </option>
-                                            <option>
+                                            <option value="name">
                                                 Name
+                                            </option>
+                                             <option value="release_date">
+                                                Release Date
                                             </option>
                                         </select>
                                     </div>
@@ -63,8 +69,10 @@
                                 </div>
                             </div>
                         </div>
-
                     </form>
+
+
+
                 </div>
             </div>
 
@@ -73,11 +81,13 @@
 
                   
                     <div class="" style="max-height:100px !important">
-                        <div class="row my-4 no-gutters pr-5 pl-2" v-for="game in games" :key="game.id" style="">
+                        <div class="row m-4 no-gutters pr-5 pl-2" v-for="game in games" :key="game.id" style="">
                             <div class="col-sm-12 col-md-2 pe-md-0" style="">
                                 <div class="bg-dark w-100 h-100 mb-5" style="min-height:100px;position:relative">
                                     <img class="card-img" src="" alt="">
-                                    <div class="m-4 d-flex badge d-md-none justify-self-center mont bg-button h4 align-self-center text-white justify-content-center align-items-center" style="width:40px;height:40px;border-radius:100%;font-size:20px;">
+                                    <div 
+                                        class="m-4 d-flex badge d-md-none justify-self-center mont bg-button h4 align-self-center text-white justify-content-center align-items-center" 
+                                        style="width:40px;height:40px;border-radius:100%;font-size:16px;">
                                         <b>
                                             {{ Math.round(game.rating * 10) / 10}}
                                         </b>
@@ -97,36 +107,15 @@
                                             {{ game.summary}}
                                         </div>
                                     </div>
-                                    <div class="mx-4 d-none d-md-flex justify-self-center mont bg-button h5 align-self-center text-white justify-content-center align-items-center" style="width:50px;height:40px;border-radius:100%">
+                                    <div 
+                                        class="mx-4 d-none d-md-flex justify-self-center mont bg-button h5 align-self-center text-white justify-content-center align-items-center" 
+                                        style="width:50px;height:40px;border-radius:100%;font-size:16px"
+                                    >
                                         <b>{{ Math.round(game.rating * 10) / 10 }}</b>
                                     </div>
                                 </button>
                             </div>
                         </div>
-
-                        
-                        <!-- <div class="row my-4 no-gutters pr-5 pl-2" :key="game.id" style="">
-                            <div class="col-sm-12 col-md-2 pe-md-0" style="">
-                                <skeleton-loader-vue class="bg-dark w-100 h-100 mb-5" style="min-height:100px;position:relative"/>
-                                    <img class="card-img" src="" alt="">
-                                     <div class="m-4 d-flex badge d-md-none justify-self-center mont bg-button h4 align-self-center text-white justify-content-center align-items-center" style="width:40px;height:40px;border-radius:100%;font-size:20px;">
-                                        <b>7</b>
-                                    </div> 
-                            </div>
-                            <div class="col-sm-12 col-md-10 ps-md-0">
-                                <button class="bg-card p-3 d-flex w-100 h-100 border-0 btn-card btn rounded-0">
-                                    <div class="text-start w-100 pr-5 p-3">
-                                        <skeleton-loader-vue class="card-title mont h5 text-white" style="line-height:20px !important"/>
-                                            <skeleton-loader-vue class="bg-text" style="font-size:13px !important"/>
-                                            <skeleton-loader-vue class="muli small bg-text text" style="font-size:12px"/>
-                                    </div>
-                                    <skeleton-loader-vue 
-                                        class="mx-4 d-none d-md-flex justify-self-center mont bg-button h5 align-self-center text-white justify-content-center align-items-center" 
-                                        style="width:50px;height:40px;border-radius:100%"
-                                    />
-                                </button>
-                            </div>
-                        </div> -->
                     </div>
 
 
@@ -159,14 +148,16 @@ export default defineComponent({
     },
     data() {
         return {
-            games: [] as Game[]
+            games: []  as Game[],
+            oldGames:[] as Game[],
+            min_score: '' as any,
+            name:'' as string
         }
     },
     methods:{
         timestamps_to_date(old_date: number){
             const date = new Date(old_date);
             const formatted_date = this.fN(date.getDate()) + '/' + this.fN(date.getMonth()+1) + '/' + this.fN(date.getFullYear())
-            console.log(formatted_date)
             return formatted_date;
         },
         fN(myNumber:number){
@@ -178,6 +169,32 @@ export default defineComponent({
         },
         truncate(str:string, n:number){
             return (str.length > n) ? str.substr(0, n-1) + '...' : str;
+        },
+        sortByScore_A(array: Game[]){
+            return array.sort(function (a:Game, b:Game) {
+                return a.rating - b.rating;
+            });
+        },
+        sortByScore_D(array:Game[]){
+            return array.sort(function (a:Game, b:Game) {
+                return b.rating - a.rating;
+            });
+        },
+        sortByMinScore(){
+
+            if(this.min_score > 0){
+                var games = []
+                for(let element of this.oldGames){
+                    if(element.rating >= this.min_score){
+                        games.push(element);
+                    }
+                }
+                this.games =  this.sortByScore_A(games);   
+            }
+            else{
+                this.games = this.oldGames
+            }
+
         }
     },
     created() {
@@ -186,6 +203,8 @@ export default defineComponent({
         gls.getAll()
             .then((response: ResponseData) => {
                 this.games = response.data
+                this.oldGames = response.data
+                localStorage.oldGames = response.data
                 console.log(response)
             })
             .catch((e: Error) => {
