87b4684e1c27beee8a8bf4c446f53c9d4502e373
Abdulmateen <jolaoshobatmat@gmail.com>
2021-12-06 19:39:07 +0100

    LAst Commit inshAllah

--- src/services/Http.ts
+++ src/services/Http.ts
@@ -2,4 +2,7 @@ import axios, { AxiosInstance } from 'axios';
 
 export const apiConnection: AxiosInstance = axios.create({
   baseURL: 'https://public.connectnow.org.uk',
+  headers:{
+    'Access-Control-Allow-Origin':'*'
+  }
 });
